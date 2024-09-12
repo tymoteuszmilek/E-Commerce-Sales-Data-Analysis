d020665ca81d9ea2356ef2ecf5af20a471edc4b2
tymoteuszmilek <146551919+tymoteuszmilek@users.noreply.github.com>
2024-09-12 18:03:19 +0200

    Delete db_scripts directory

--- db_scripts/run_all_queries.sql
+++
@@ -1,8 +0,0 @@
-\i 'order_status_queries.sql';
-\i 'customer_analysis.sql';
-\i 'category_sales.sql';
-\i 'sales_distribution.sql';
-\i 'sku_sales.sql';
-\i 'size_sales.sql';
-\i 'international_sales.sql';
-\i 'comparison_queries.sql'
--- db_scripts/run_initializer.sql
+++
@@ -1,4 +0,0 @@
-\i 'create_tables.sql';
-\i 'insert_data.sql';
-\i 'create_views.sql';
-\i 'create_indexes.sql'
